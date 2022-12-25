d5fffe1c62f1c34a5387ecaf382ea18a7c068ba6
noobo <nooboboo2@gmail.com>
2022-12-25 13:28:39 +0000

    Initial commit

---
+++ src/main/java/net/noobo/orangemod/OrangeModClient.java
@@ -0,0 +1,10 @@
+package net.noobo.orangemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OrangeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
